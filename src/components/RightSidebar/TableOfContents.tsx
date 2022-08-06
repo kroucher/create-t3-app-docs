f904ca7f1bdb48346b8941878aadff6c9cc3c931
sir-mr-bean <kroucher.1019@gmail.com>
2022-08-06 12:03:53 +1000

    add prettier, restyle right menu

--- src/components/RightSidebar/TableOfContents.tsx
+++ src/components/RightSidebar/TableOfContents.tsx
@@ -23,7 +23,7 @@ const TableOfContents = ({ headings }: { headings: MarkdownHeading[] }) => {
 
   return (
     <div className="w-full">
-      <h2 className="text-lg my-4">On this page</h2>
+      <h2 className="text-lg my-4 font-semibold">On this page</h2>
       <ul className="border-l-2 border-slate-800 dark:border-brand-primary my-1">
         <li
           className={`pl-4 depth-2 ${
